84fcc1d244793c415cb45ef721222b1b00dd18fc
lmartns <leonardomartins578@gmail.com>
2024-06-19 16:13:08 -0300

    feat: card, avatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
-import Header from '@/components/custom/header';
+"use client";
+
+import React from "react";
+import Header from "@/components/custom/header";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 const Home = () => {
   return (
     <div>
-      <Header/>
+      <Header />
+      <div className="content-wrapper pt-10">
+        <Avatar>
+          <AvatarImage
+            className="w-20 h-20 rounded-full mb-3"
+            src="https://github.com/lmartns.png"
+          />
+        </Avatar>
+        <h1 className="text-center text-3xl font-bold">
+          Welcome to my portfolio
+        </h1>
+        <Card className=" w-full h-auto rounded-sm mt-6">
+          <CardHeader>
+            <CardTitle className="text-2xl">Links</CardTitle>
+          </CardHeader>
+          <CardContent className="flex items-center justify-around">
+            <p>teste</p>
+            <p>teste</p>
+            <p>teste</p>
+          </CardContent>
+        </Card>
+        <Card className=" w-full h-auto rounded-sm mt-6">
+          <CardHeader>
+            <CardTitle className="text-2xl">Projects</CardTitle>
+          </CardHeader>
+        </Card>
+      </div>
     </div>
   );
 };
--- src/components/custom/header.tsx
+++ src/components/custom/header.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import Circle from './circle';
+import React from "react";
+import Circle from "./circle";
 
 const Header = () => {
   return (
-    <header className="w-full border-b">
+    <header className="w-full border-b mb-10">
       <div className="flex h-14 items-center px-4">
         <Circle />
-        <div className="text-dark ml-1">Leonardo Martins</div>
+        <div className="text-dark ml-2">Leonardo Martins</div>
       </div>
     </header>
   );
