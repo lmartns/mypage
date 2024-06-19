0c626c89e75758167c1c408f2c6672a1d7d71220
lmartns <leonardomartins578@gmail.com>
2024-06-19 17:05:24 -0300

    feat: card, avatar

--- src/components/custom/header.tsx
+++ src/components/custom/header.tsx
@@ -3,7 +3,7 @@ import Circle from "./circle";
 
 const Header = () => {
   return (
-    <header className="w-full border-b mb-4">
+    <header className="w-full mb-4">
       <div className="flex h-14 items-center px-4">
         <div className="text-dark ml-1">Leonardo Martins</div>
       </div>
