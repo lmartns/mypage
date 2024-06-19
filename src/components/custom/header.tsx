97a655b83dbffd254d3c5b405d6bc87ace302ec9
lmartns <leonardomartins578@gmail.com>
2024-06-19 13:46:10 -0300

    First commit

---
+++ src/components/custom/header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="w-full border-b">
+      <div className="flex h-14 items-center px-4">
+        <div className="text-dark">Leonardo Martins</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
